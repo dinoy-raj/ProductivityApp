d23678e1fcbce18b38f2f393484c33c8d5914fc6
amalnathm7 <amalnathm7@gmail.com>
2021-06-29 15:48:01 +0530

    UI changes

--- app/lib/screens/projects/projectscreen.dart
+++ app/lib/screens/projects/projectscreen.dart
@@ -17,119 +17,121 @@ class _TeamScreenState extends State<TeamScreen> {
       backgroundColor: Colors.white,
       body: Padding(
         padding: const EdgeInsets.only(top: 60, left: 30, right: 30),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              child: Align(
-                alignment: Alignment.topLeft,
-                child: Text(
-                  "Your\nProjects",
-                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                child: Align(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    "Your\nProjects",
+                    style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                  ),
                 ),
               ),
-            ),
-            Text(
-              "Add and edit your projects",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 15,
-                color: Colors.grey,
+              Text(
+                "Add and edit your projects",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15,
+                  color: Colors.grey,
+                ),
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-              height: 100,
-              decoration: BoxDecoration(
-                  shape: BoxShape.rectangle,
-                  borderRadius: BorderRadius.circular(20),
-                  color: Colors.white,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(.1),
-                      blurRadius: 80,
-                      spreadRadius: 2,
-                      offset: Offset(0, 3),
-                    ),
-                  ]),
-              child: Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 8.0, right: 8.0),
-                    child: Container(
-                      height: 80,
-                      width: 80,
-                      child: Lottie.network(
-                          "https://assets1.lottiefiles.com/packages/lf20_yZ9gAl.json"),
-                    ),
-                  ),
-                  SizedBox(width: 5,),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Have a new one?",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 20),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: 100,
+                decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(.1),
+                        blurRadius: 80,
+                        spreadRadius: 2,
+                        offset: Offset(0, 3),
                       ),
-                      Text(
-                        "Add new project   ->",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 12,
-                          color: Colors.grey,
+                    ]),
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                      child: Container(
+                        height: 80,
+                        width: 80,
+                        child: Lottie.network(
+                            "https://assets1.lottiefiles.com/packages/lf20_yZ9gAl.json"),
+                      ),
+                    ),
+                    SizedBox(width: 5,),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Have a new one?",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 20),
                         ),
+                        Text(
+                          "Add new project   ->",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 12,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Container(
+                      height: 40,
+                      width: 40,
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(5),
                       ),
-                    ],
-                  ),
-                  SizedBox(
-                    width: 20,
-                  ),
-                  Container(
-                    height: 40,
-                    width: 40,
-                    decoration: BoxDecoration(
-                      color: Colors.black,
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                    child: IconButton(
-                      onPressed: () {
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => ProjectEditing()));
-                      },
-                      icon: Icon(Icons.add),
-                      color: Colors.white,
-                    ),
-                  )
-                ],
+                      child: IconButton(
+                        onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => ProjectEditing()));
+                        },
+                        icon: Icon(Icons.add),
+                        color: Colors.white,
+                      ),
+                    )
+                  ],
+                ),
               ),
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            Text(
-              "   Owned Projects",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 18,
-                color: Colors.grey[700],
+              SizedBox(
+                height: 40,
               ),
-            ),
-            SizedBox(
-              height: 120,
-            ),
-            Text(
-              "   Other Projects",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 18,
-                color: Colors.grey[700],
+              Text(
+                "   Owned Projects",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                  color: Colors.grey[700],
+                ),
               ),
-            ),
-          ],
+              SizedBox(
+                height: 120,
+              ),
+              Text(
+                "   Other Projects",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                  color: Colors.grey[700],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
