daeebd8d77f650c2f96e5c410d785239e5f11c79
amalnathm7 <amalnathm7@gmail.com>
2021-06-30 00:15:54 +0530

    DB Changes

--- app/lib/screens/projects/projectscreen.dart
+++ app/lib/screens/projects/projectscreen.dart
@@ -1,5 +1,4 @@
 import 'dart:math';
-
 import 'package:app/screens/projects/project_edits.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
