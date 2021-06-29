f75331e125d732110ab83037641961aedb2f4fe6
Dinoy-Raj <dinoykraj.com>
2021-06-29 16:04:39 +0530

    edit notes page updated

--- app/lib/screens/note/note_edits.dart
+++ app/lib/screens/note/note_edits.dart
@@ -11,6 +11,7 @@ class NoteEditing extends StatefulWidget {
 }
 
 class _NoteEditingState extends State<NoteEditing> {
+  final _formKey = GlobalKey<FormState>();
   Future<void> updateData() async {
     Map<String, dynamic> data = {
       "body": _bodyController.text,
@@ -47,148 +48,129 @@ class _NoteEditingState extends State<NoteEditing> {
       ),
       body: SingleChildScrollView(
         physics: BouncingScrollPhysics(),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Center(
-              child: Text(
-                "Add Note",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Align(
+                alignment: Alignment.topLeft,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 25.0, bottom: 20),
+                  child: Text(
+                    "Add Note",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                  ),
+                ),
               ),
-            ),
-            Container(
-              width: screenWidth,
-              height: screenHeight * .2,
-              child: Padding(
-                padding: const EdgeInsets.only(
-                    left: 25, right: 15, top: 8, bottom: 8),
-                child: Align(
-                    alignment: Alignment.topLeft,
-                    child: TextFormField(
-                      // validator: (String? value) {
-                      //   return value == null ? "Field Cannot Be Empty" : null;
-                      // },
-                      controller: _titleController,
-                      maxLines: 3,
-                      //autocorrect: true,
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                      decoration: InputDecoration(
-                        hintText: "Title",
-                        border: InputBorder.none,
-                        focusedBorder: InputBorder.none,
-                        enabledBorder: InputBorder.none,
-                        errorBorder: InputBorder.none,
-                        disabledBorder: InputBorder.none,
-                      ),
-                    )),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 20, right: 20),
-              child: Container(
-                decoration: BoxDecoration(
-                    shape: BoxShape.rectangle,
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.white,
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(.1),
-                        blurRadius: 100,
-                        spreadRadius: 2,
-                        offset: Offset(0, 3),
-                      ),
-                    ]
-                    //borderRadius: BorderRadius.circular(10),
-                    ),
+              Container(
                 width: screenWidth,
-                height: screenHeight * .8,
+                height: screenHeight * .3,
                 child: Padding(
                   padding: const EdgeInsets.only(
                       left: 25, right: 15, top: 8, bottom: 8),
                   child: Align(
                       alignment: Alignment.topLeft,
-                      child: TextField(
-                        maxLines: 10,
-                        controller: _bodyController,
+                      child: TextFormField(
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        validator: (String? value) {
+                          return value == null || value.trim().isEmpty
+                              ? ""
+                              : null;
+                        },
+                        controller: _titleController,
+                        maxLines: 3,
+                        autofocus: true,
                         //autocorrect: true,
-                        style: TextStyle(fontSize: 15),
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
                         decoration: InputDecoration(
-                          hintText: "Content",
+                          hintText: "Title",
                           border: InputBorder.none,
                           focusedBorder: InputBorder.none,
                           enabledBorder: InputBorder.none,
-                          errorBorder: InputBorder.none,
+                          errorBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.red,
+                            width: 2
+                            ),
+                          ),
                           disabledBorder: InputBorder.none,
                         ),
                       )),
                 ),
               ),
-            ),
-            SizedBox(
-              height: screenHeight * .15,
-            ),
-            Container(
-              height: 40,
-              width: screenWidth * .4,
-              child: ElevatedButton(
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.black),
-                  ),
-                  onPressed: () {
-                    if (_titleController.text != "" &&
-                        _bodyController.text != "") {
-                      updateData();
-                      Navigator.pop(context);
-                    } else {
-                      showModalBottomSheet(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return Container(
-                              height: 200,
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Text(
-                                    "Title and Content should not be empty!",
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.red,
-                                      fontSize: 20,
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    height: 50,
-                                  ),
-                                  OutlineButton(
-                                    onPressed: () {
-                                      Navigator.pop(context);
-                                    },
-                                    child: Text("Cancel"),
-                                  )
-                                ],
-                              ),
-                            );
-                          });
-                    }
-                  },
-                  child: Text(
-                    "Add Note",
-                    style: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.bold,
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20),
+                child: Container(
+                  decoration: BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      borderRadius: BorderRadius.circular(20),
                       color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(.1),
+                          blurRadius: 100,
+                          spreadRadius: 2,
+                          offset: Offset(0, 3),
+                        ),
+                      ]),
+                  width: screenWidth,
+                  height: screenHeight * .8,
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        left: 25, right: 15, top: 8, bottom: 8),
+                    child: Align(
+                        alignment: Alignment.topLeft,
+                        child: TextFormField(
+                          maxLines: 10,
+                          controller: _bodyController,
+                          style: TextStyle(fontSize: 15),
+                          decoration: InputDecoration(
+                            hintText: "Content",
+                            border: InputBorder.none,
+                            focusedBorder: InputBorder.none,
+                            enabledBorder: InputBorder.none,
+                            errorBorder: InputBorder.none,
+                            disabledBorder: InputBorder.none,
+                          ),
+                        )),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: screenHeight * .15,
+              ),
+              Container(
+                  height: 40,
+                  width: screenWidth * .4,
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.black),
+                    ),
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        updateData();
+                        Navigator.pop(context);
+                      }
+                    },
+                    child: Text(
+                      "Add Note",
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
                     ),
                   )),
-            ),
-            SizedBox(
-              height: screenHeight * .3,
-            )
-          ],
+              SizedBox(
+                height: screenHeight * .3,
+              )
+            ],
+          ),
         ),
       ),
     );
