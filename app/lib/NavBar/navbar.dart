3db87d6a56111d0c022d5bd0af9e266d1db15608
amalnathm7 <amalnathm7@gmail.com>
2021-06-29 10:28:36 +0530

    UI changes

--- app/lib/NavBar/navbar.dart
+++ app/lib/NavBar/navbar.dart
@@ -1,7 +1,7 @@
 import 'package:app/screens/main/mainscreens.dart';
 import 'package:app/screens/note/notescreen.dart';
 import 'package:app/screens/todo/todoscreen.dart';
-import 'package:app/screens/team/teamScreeen.dart';
+import 'package:app/screens/projects/projectscreen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
---
+++ app/lib/screens/projects/projectscreen.dart
@@ -0,0 +1,115 @@
+import 'package:app/screens/projects/project_edits.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class TeamScreen extends StatefulWidget {
+  const TeamScreen({Key? key}) : super(key: key);
+
+  @override
+  _TeamScreenState createState() => _TeamScreenState();
+}
+
+class _TeamScreenState extends State<TeamScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Padding(
+          padding: const EdgeInsets.only(top: 60, left: 30, right: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                child: Align(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    "Your\nProjects",
+                    style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              Text(
+                "Add and Edit your Projects",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15,
+                  color: Colors.grey,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(.1),
+                        blurRadius: 80,
+                        spreadRadius: 2,
+                        offset: Offset(0, 3),
+                      ),
+                    ]),
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: Container(
+                        height: 80,
+                        width: 80,
+                        child: Lottie.network(
+                            "https://assets6.lottiefiles.com/packages/lf20_wd1udlcz.json"),
+                      ),
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Have A New One?",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 20),
+                        ),
+                        Text(
+                          "Add New Note   ->",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 10,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    Container(
+                      height: 40,
+                      width: 40,
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: IconButton(
+                        onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => ProjectEditing()));
+                        },
+                        icon: Icon(Icons.add),
+                        color: Colors.white,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
