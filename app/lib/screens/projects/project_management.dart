da8efb975769d0aaac49f29cb4f0e355d5eaeeac
amalnathm7 <amalnathm7@gmail.com>
2021-06-30 21:58:59 +0530

    Project screen

--- app/lib/screens/projects/project_management.dart
+++ app/lib/screens/projects/project_management.dart
@@ -18,7 +18,6 @@ class ProjectManagement extends StatefulWidget {
 class _ProjectState extends State<ProjectManagement> {
   _ProjectState({this.project});
   User _user = FirebaseAuth.instance.currentUser!;
-
   final Project? project;
 
   @override
@@ -62,25 +61,98 @@ class _ProjectState extends State<ProjectManagement> {
                     ),
                   ),
                 )
-              : IconButton(
-                  splashColor: Colors.transparent,
-                  highlightColor: Colors.transparent,
-                  onPressed: () {},
-                  icon: Tooltip(
-                    message: "Collaborators",
-                    child: Icon(
-                      Icons.people,
-                      color: Colors.grey[700],
-                    ),
-                  ),
-                )
+              : SizedBox(),
+          IconButton(
+            splashColor: Colors.transparent,
+            highlightColor: Colors.transparent,
+            onPressed: () {
+              Scaffold.of(context).openEndDrawer();
+            },
+            icon: Tooltip(
+              message: "Collaborators",
+              child: Icon(
+                Icons.people,
+                color: Colors.grey[700],
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 10,
+          )
         ],
+      ),
+      endDrawer: Drawer(
+        child: Column(
+          children: [
+            DrawerHeader(child: Text("Collaborators")),
+            ListView.builder(
+                itemCount: project!.collab!.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: project!.collab![index]['name'],
+                  );
+                }),
+          ],
+        ),
       ),
       floatingActionButton: Padding(
         padding: const EdgeInsets.all(20),
         child: NeumorphicFloatingActionButton(
           child: Icon(Icons.call),
           onPressed: () {},
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 20, right: 20),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    project!.title!,
+                    overflow: TextOverflow.clip,
+                    style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey[900]),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[700],
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 10, right: 10),
+                      child: Text(
+                        project!.type!,
+                        style: TextStyle(fontSize: 16, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5),
+                border: Border.all(color: Colors.grey[700]!)
+              ),
+              width: MediaQuery.of(context).size.width,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  project!.body!,
+                  style: TextStyle(fontSize: 12, color: Colors.grey[700]),
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
