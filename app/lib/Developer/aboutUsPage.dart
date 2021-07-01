53039251c834bc12a559c9b49735337c5c1b5e07
Dinoy-Raj <dinoykraj.com>
2021-07-01 19:02:24 +0530

    about us updated

--- app/lib/Developer/aboutUsPage.dart
+++ app/lib/Developer/aboutUsPage.dart
@@ -16,38 +16,190 @@ class _AboutUsState extends State<AboutUs> {
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.width;
     return Scaffold(
-
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Colors.white10,
         leading: IconButton(
-            splashRadius: .5,
+            splashRadius: 20,
             onPressed: () {
               Navigator.pop(context);
             },
             icon: Tooltip(
               message: "Exit Without Saving",
               child: Icon(
-                Icons.cancel,
+                Icons.arrow_back_ios_outlined,
                 color: Colors.black,
               ),
             )),
       ),
       body: SingleChildScrollView(
-        physics: BouncingScrollPhysics(),
-        child: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-
-            children: [
-              Container(
-                  height: screenHeight * .3,
+          physics: BouncingScrollPhysics(),
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                    height: screenHeight * .3,
+                    width: screenWidth,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.rectangle,
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.black,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(.1),
+                            blurRadius: 100,
+                            spreadRadius: 2,
+                            offset: Offset(0, 3),
+                          ),
+                        ]
+                        //borderRadius: BorderRadius.circular(10),
+                        ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      //crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          height: 150,
+                          width: 150,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                FeatherIcons.checkCircle,
+                                color: Colors.white,
+                                size: 30,
+                              ),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Text(
+                                "Lets Do It",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          height: screenHeight * .3,
+                          width: 1,
+                          color: Colors.grey,
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              height: 2,
+                              width: 20,
+                              color: Colors.white,
+                            ),
+                            Container(
+                              height: 20,
+                              child: DefaultTextStyle(
+                                style: const TextStyle(
+                                  fontSize: 15.0,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.grey,
+                                ),
+                                child: AnimatedTextKit(
+                                  repeatForever: true,
+                                  animatedTexts: [
+                                    FadeAnimatedText('do IT!'),
+                                    FadeAnimatedText('do it RIGHT!!'),
+                                    FadeAnimatedText('do it RIGHT NOW!!!'),
+                                  ],
+                                  onTap: () {
+                                    print("Tap Event");
+                                  },
+                                ),
+                              ),
+                            ),
+                            Text(
+                              "Complete Analysis Of Your Activities",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 8,
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    )),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  height: screenHeight * .35,
                   width: screenWidth,
                   decoration: BoxDecoration(
                       shape: BoxShape.rectangle,
                       borderRadius: BorderRadius.circular(10),
-                      color: Colors.black,
+                      color: Colors.white10,
+                      border: Border.all(color: Colors.white,width: 1),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(.1),
+                          blurRadius: 100,
+                          spreadRadius: 2,
+                          offset: Offset(0, 3),
+                        ),
+                      ]
+                      //borderRadius: BorderRadius.circular(10),
+                      ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Row(
+                        children: [
+                         Container(
+                           height: 20,
+                           width: 5,
+                           decoration:BoxDecoration(
+                               shape: BoxShape.rectangle,
+                               borderRadius: BorderRadius.circular(10),
+                               color: Colors.white10,
+                               border: Border.all(color: Colors.white,width: 1),
+                               boxShadow: [
+                                 BoxShadow(
+                                   color: Colors.black.withOpacity(.1),
+                                   blurRadius: 100,
+                                   spreadRadius: 2,
+                                   offset: Offset(0, 3),
+                                 ),
+                               ]
+                             //borderRadius: BorderRadius.circular(10),
+                           ),
+
+                         ),
+                          Container(
+                            width: 10,
+
+                          ),
+                          Container(
+                             height: screenHeight * .2,
+                             width: 200,
+                            child: ,
+                          )
+
+                        ],
+                      ),
+                    ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  height: screenHeight ,
+                  width: screenWidth,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.white,
                       boxShadow: [
                         BoxShadow(
                           color: Colors.black.withOpacity(.1),
@@ -58,85 +210,10 @@ class _AboutUsState extends State<AboutUs> {
                       ]
                     //borderRadius: BorderRadius.circular(10),
                   ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    //crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Container(
-                        height: 150,
-                        width: 150,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(
-                              FeatherIcons.checkCircle,
-                              color: Colors.white,
-                              size: 30,
-                            ),
-                            SizedBox(
-                              height: 5,
-                            ),
-                            Text(
-                              "Lets Do It",
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 20),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        height: screenHeight * .3,
-                        width: 1,
-                        color: Colors.grey,
-                      ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            height: 2,
-                            width: 20,
-                            color: Colors.white,
-                          ),
-                          Container(
-                            height: 20,
-                            child: DefaultTextStyle(
-                              style: const TextStyle(
-                                fontSize: 15.0,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.grey,
-                              ),
-                              child: AnimatedTextKit(
-                                repeatForever: true,
-                                animatedTexts: [
-                                  FadeAnimatedText('do IT!'),
-                                  FadeAnimatedText('do it RIGHT!!'),
-                                  FadeAnimatedText('do it RIGHT NOW!!!'),
-                                ],
-                                onTap: () {
-                                  print("Tap Event");
-                                },
-                              ),
-                            ),
-                          ),
-                          Text(
-                            "Complete Analysis Of Your Activities",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 8,
-                              color: Colors.grey,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  )),
-            ],
-          ),
-        )
-      ),
+                ),
+              ],
+            ),
+          )),
     );
   }
 }
