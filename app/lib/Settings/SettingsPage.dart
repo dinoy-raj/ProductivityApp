86b5093649085a791d6305b702f57ca9e35ee727
Dinoy-Raj <dinoykraj.com>
2021-07-02 15:57:36 +0530

    settings page ui updated , notes page finished

--- app/lib/Settings/SettingsPage.dart
+++ app/lib/Settings/SettingsPage.dart
@@ -1,5 +1,7 @@
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({Key? key}) : super(key: key);
@@ -9,8 +11,11 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
+  bool val = false;
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
@@ -21,7 +26,7 @@ class _SettingsPageState extends State<SettingsPage> {
               Navigator.pop(context);
             },
             icon: Tooltip(
-              message: "Exit Without Saving",
+              message: "Exit Settings",
               child: Icon(
                 Icons.cancel,
                 color: Colors.black,
@@ -30,7 +35,152 @@ class _SettingsPageState extends State<SettingsPage> {
       ),
       body: SingleChildScrollView(
           child: Center(
-        child: Text("Settings"),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Container(
+                height: screenHeight * .8,
+                width: screenWidth,
+                decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.white10,
+                    border: Border.all(color: Colors.white, width: 1),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(.1),
+                        blurRadius: 100,
+                        spreadRadius: 2,
+                        offset: Offset(0, 3),
+                      ),
+                    ]
+                    //borderRadius: BorderRadius.circular(10),
+                    ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 80,
+                      width: screenWidth * .8,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.settings,
+                            size: 14,
+                            color: Colors.black.withOpacity(.5),
+                          ),
+                          Container(
+                            width: 5,
+                          ),
+                          Text(
+                            "Settings",
+                            style: TextStyle(
+                                color: Colors.black.withOpacity(.5),
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Divider(),
+                    Container(
+                      height: screenHeight * .6,
+                      width: screenWidth * .8,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.rectangle,
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.white.withOpacity(.8),
+                        //border: Border.all(color: Colors.white, width: 1),
+                        // boxShadow: [
+                        //   BoxShadow(
+                        //     color: Colors.black.withOpacity(.1),
+                        //     blurRadius: 100,
+                        //     spreadRadius: 2,
+                        //     offset: Offset(0, 3),
+                        //   ),
+                        // ]
+                        //borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                                shape: BoxShape.rectangle,
+                                borderRadius: BorderRadius.circular(10),
+                                color: Colors.white10,
+                                border:
+                                    Border.all(color: Colors.white, width: 1),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black.withOpacity(.1),
+                                    blurRadius: 100,
+                                    spreadRadius: 2,
+                                    offset: Offset(0, 3),
+                                  ),
+                                ]
+                                //borderRadius: BorderRadius.circular(10),
+                                ),
+                            height: 50,
+                            width: screenWidth * .7,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Container(
+                                    height: 20,
+                                    width: 100,
+                                    child: Text(
+                                      "Dark Mode",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 15,
+                                          color: Colors.grey),
+                                    ),
+                                  ),
+                                  Container(
+                                    height: 20,
+                                    child: CupertinoSwitch(
+                                      value: false,
+                                      onChanged: (bool val) {
+                                        setState(() {
+                                          val = !val;
+                                        });
+                                      },
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      height: screenHeight * .06,
+                      width: screenWidth * .8,
+                      child: Center(
+                        child: Text(
+                          "* Most Of The Features Are Awaited, Soon We Will Work On It And Update Settings",
+                          style: TextStyle(
+                              fontSize: 10,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey.withOpacity(.5)),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       )),
     );
   }
--- app/lib/screens/todo/todo_edits.dart
+++ app/lib/screens/todo/todo_edits.dart
@@ -1,193 +1,193 @@
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:flutter/material.dart';
-//
-// class TodoEditing extends StatefulWidget {
-//   const TodoEditing({Key? key}) : super(key: key);
-//
-//   @override
-//   _TodoEditingState createState() => _TodoEditingState();
-// }
-//
-// class _TodoEditingState extends State<TodoEditing> {
-//   Future<void> updateData() async {
-//     Map<String, dynamic> data = {
-//       "body": _bodyController.text,
-//       "title": _titleController.text
-//     };
-//     CollectionReference collectionReference =
-//         FirebaseFirestore.instance.collection("todo");
-//     await collectionReference.add(data);
-//   }
-//
-//   TextEditingController _titleController = TextEditingController();
-//   TextEditingController _bodyController = TextEditingController();
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     double screenWidth = MediaQuery.of(context).size.width;
-//     double screenHeight = MediaQuery.of(context).size.width;
-//     return Scaffold(
-//       appBar: AppBar(
-//         elevation: 0,
-//         backgroundColor: Colors.white10,
-//         leading: IconButton(
-//             onPressed: () {
-//               Navigator.pop(context);
-//             },
-//             icon: Tooltip(
-//               message: "Exit Without Saving",
-//               child: Icon(
-//                 Icons.cancel,
-//                 color: Colors.black,
-//               ),
-//             )),
-//       ),
-//       body: SingleChildScrollView(
-//         physics: BouncingScrollPhysics(),
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           crossAxisAlignment: CrossAxisAlignment.center,
-//           children: [
-//             Center(
-//               child: Text(
-//                 "Add Todo",
-//                 style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
-//               ),
-//             ),
-//             Container(
-//               width: screenWidth,
-//               height: screenHeight * .2,
-//               child: Padding(
-//                 padding: const EdgeInsets.only(
-//                     left: 25, right: 15, top: 8, bottom: 8),
-//                 child: Align(
-//                     alignment: Alignment.topLeft,
-//                     child: TextFormField(
-//                       validator: (String? value) {
-//                         return value == null ? "Field Cannot Be Empty" : null;
-//                       },
-//                       controller: _titleController,
-//                       maxLines: 5,
-//                       //autocorrect: true,
-//                       style:
-//                           TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-//                       decoration: InputDecoration(
-//                         hintText: "Title",
-//                         border: InputBorder.none,
-//                         focusedBorder: InputBorder.none,
-//                         enabledBorder: InputBorder.none,
-//                         errorBorder: InputBorder.none,
-//                         disabledBorder: InputBorder.none,
-//                       ),
-//                     )),
-//               ),
-//             ),
-//             SizedBox(
-//               height: 20,
-//             ),
-//             Padding(
-//               padding: const EdgeInsets.only(left: 20, right: 20),
-//               child: Container(
-//                 decoration: BoxDecoration(
-//                     shape: BoxShape.rectangle,
-//                     borderRadius: BorderRadius.circular(20),
-//                     color: Colors.white,
-//                     boxShadow: [
-//                       BoxShadow(
-//                         color: Colors.black.withOpacity(.1),
-//                         blurRadius: 100,
-//                         spreadRadius: 2,
-//                         offset: Offset(0, 3),
-//                       ),
-//                     ]
-//                     //borderRadius: BorderRadius.circular(10),
-//                     ),
-//                 width: screenWidth,
-//                 height: screenHeight * .8,
-//                 child: Padding(
-//                   padding: const EdgeInsets.only(
-//                       left: 25, right: 15, top: 8, bottom: 8),
-//                   child: Align(
-//                       alignment: Alignment.topLeft,
-//                       child: TextField(
-//                         maxLines: 10,
-//                         controller: _bodyController,
-//                         //autocorrect: true,
-//                         style: TextStyle(fontSize: 15),
-//                         decoration: InputDecoration(
-//                           hintText: "Content",
-//                           border: InputBorder.none,
-//                           focusedBorder: InputBorder.none,
-//                           enabledBorder: InputBorder.none,
-//                           errorBorder: InputBorder.none,
-//                           disabledBorder: InputBorder.none,
-//                         ),
-//                       )),
-//                 ),
-//               ),
-//             ),
-//             SizedBox(
-//               height: screenHeight * .15,
-//             ),
-//             Container(
-//               height: 40,
-//               width: screenWidth * .4,
-//               child: ElevatedButton(
-//                   style: ButtonStyle(
-//                     backgroundColor: MaterialStateProperty.all(Colors.black),
-//                   ),
-//                   onPressed: () {
-//                     if (_titleController.text != "" &&
-//                         _bodyController.text != "") {
-//                       updateData();
-//                       Navigator.pop(context);
-//                     } else {
-//                       showModalBottomSheet(
-//                           context: context,
-//                           builder: (BuildContext context) {
-//                             return Container(
-//                               height: 200,
-//                               child: Column(
-//                                 mainAxisAlignment: MainAxisAlignment.center,
-//                                 children: [
-//                                   Text(
-//                                     "Title and Content should not be empty!",
-//                                     style: TextStyle(
-//                                       fontWeight: FontWeight.bold,
-//                                       color: Colors.red,
-//                                       fontSize: 20,
-//                                     ),
-//                                   ),
-//                                   SizedBox(
-//                                     height: 50,
-//                                   ),
-//                                   OutlineButton(
-//                                     onPressed: () {
-//                                       Navigator.pop(context);
-//                                     },
-//                                     child: Text("Cancel"),
-//                                   )
-//                                 ],
-//                               ),
-//                             );
-//                           });
-//                     }
-//                   },
-//                   child: Text(
-//                     "Add Todo",
-//                     style: TextStyle(
-//                       fontSize: 14,
-//                       fontWeight: FontWeight.bold,
-//                       color: Colors.white,
-//                     ),
-//                   )),
-//             ),
-//             SizedBox(
-//               height: screenHeight * .3,
-//             )
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class TodoEditing extends StatefulWidget {
+  const TodoEditing({Key? key}) : super(key: key);
+
+  @override
+  _TodoEditingState createState() => _TodoEditingState();
+}
+
+class _TodoEditingState extends State<TodoEditing> {
+  Future<void> updateData() async {
+    Map<String, dynamic> data = {
+      "body": _bodyController.text,
+      "title": _titleController.text
+    };
+    CollectionReference collectionReference =
+        FirebaseFirestore.instance.collection("todo");
+    await collectionReference.add(data);
+  }
+
+  TextEditingController _titleController = TextEditingController();
+  TextEditingController _bodyController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white10,
+        leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: Tooltip(
+              message: "Exit Without Saving",
+              child: Icon(
+                Icons.cancel,
+                color: Colors.black,
+              ),
+            )),
+      ),
+      body: SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+              child: Text(
+                "Add Todo",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+              ),
+            ),
+            Container(
+              width: screenWidth,
+              height: screenHeight * .2,
+              child: Padding(
+                padding: const EdgeInsets.only(
+                    left: 25, right: 15, top: 8, bottom: 8),
+                child: Align(
+                    alignment: Alignment.topLeft,
+                    child: TextFormField(
+                      validator: (String? value) {
+                        return value == null ? "Field Cannot Be Empty" : null;
+                      },
+                      controller: _titleController,
+                      maxLines: 5,
+                      //autocorrect: true,
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                      decoration: InputDecoration(
+                        hintText: "Title",
+                        border: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                        enabledBorder: InputBorder.none,
+                        errorBorder: InputBorder.none,
+                        disabledBorder: InputBorder.none,
+                      ),
+                    )),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20),
+              child: Container(
+                decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(.1),
+                        blurRadius: 100,
+                        spreadRadius: 2,
+                        offset: Offset(0, 3),
+                      ),
+                    ]
+                    //borderRadius: BorderRadius.circular(10),
+                    ),
+                width: screenWidth,
+                height: screenHeight * .8,
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                      left: 25, right: 15, top: 8, bottom: 8),
+                  child: Align(
+                      alignment: Alignment.topLeft,
+                      child: TextField(
+                        maxLines: 10,
+                        controller: _bodyController,
+                        //autocorrect: true,
+                        style: TextStyle(fontSize: 15),
+                        decoration: InputDecoration(
+                          hintText: "Content",
+                          border: InputBorder.none,
+                          focusedBorder: InputBorder.none,
+                          enabledBorder: InputBorder.none,
+                          errorBorder: InputBorder.none,
+                          disabledBorder: InputBorder.none,
+                        ),
+                      )),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: screenHeight * .15,
+            ),
+            Container(
+              height: 40,
+              width: screenWidth * .4,
+              child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.black),
+                  ),
+                  onPressed: () {
+                    if (_titleController.text != "" &&
+                        _bodyController.text != "") {
+                      updateData();
+                      Navigator.pop(context);
+                    } else {
+                      showModalBottomSheet(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return Container(
+                              height: 200,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    "Title and Content should not be empty!",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.red,
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 50,
+                                  ),
+                                  OutlineButton(
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: Text("Cancel"),
+                                  )
+                                ],
+                              ),
+                            );
+                          });
+                    }
+                  },
+                  child: Text(
+                    "Add Todo",
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  )),
+            ),
+            SizedBox(
+              height: screenHeight * .3,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
