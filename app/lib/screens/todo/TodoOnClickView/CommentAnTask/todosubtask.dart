ac2731734a53322044dd051690ed3c0344fc6941
Dinoy-Raj <dinoykraj.com>
2021-07-05 22:34:39 +0530

    added sub todo screen

--- app/lib/screens/todo/TodoOnClickView/CommentAnTask/todosubtask.dart
+++ app/lib/screens/todo/TodoOnClickView/CommentAnTask/todosubtask.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:app/screens/splashscreen/splash_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -16,6 +18,7 @@ class TodoSubTask extends StatefulWidget {
 
 class _TodoSubTaskState extends State<TodoSubTask> {
   _TodoSubTaskState(this.data);
+  final _formKey = GlobalKey<FormState>();
   Map<String, dynamic> data;
   String id = "";
   initState() {
@@ -33,186 +36,238 @@ class _TodoSubTaskState extends State<TodoSubTask> {
         .collection("todo")
         .doc(id)
         .collection("subtask")
+        .orderBy("no")
         .snapshots();
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          Container(
-            height: 50,
-            width: widget.screenWidth,
-            decoration: BoxDecoration(
-                shape: BoxShape.rectangle,
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.white10,
-                border: Border.all(color: Colors.white, width: 1),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(.1),
-                    blurRadius: 100,
-                    spreadRadius: 2,
-                    offset: Offset(0, 3),
-                  ),
-                ]
-                //borderRadius: BorderRadius.circular(10),
-                ),
-            child: Padding(
-              padding: const EdgeInsets.all(2.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                    width: widget.screenWidth * .4,
-                    child: TextFormField(
-                      maxLines: 1,
-                      controller: _subtaskController,
-                      style: TextStyle(
-                        fontSize: 15,
-                        color: Colors.black.withOpacity(.6),
-                        fontWeight: FontWeight.bold,
+    return GestureDetector(
+      onTap: () {
+        FocusScopeNode currentFocus = FocusScopeNode();
+        if (!currentFocus.hasPrimaryFocus) {
+          currentFocus.unfocus();
+        }
+      },
+      child: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              Container(
+                height: 50,
+                width: widget.screenWidth,
+                decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.white10,
+                    border: Border.all(color: Colors.white, width: 1),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(.1),
+                        blurRadius: 100,
+                        spreadRadius: 2,
+                        offset: Offset(0, 3),
                       ),
-                      decoration: InputDecoration(
-                        hintText: "Add Tasks",
-                        border: InputBorder.none,
-                        focusedBorder: InputBorder.none,
-                        enabledBorder: InputBorder.none,
-                        errorBorder: InputBorder.none,
-                        disabledBorder: InputBorder.none,
-                      ),
+                    ]
+                    //borderRadius: BorderRadius.circular(10),
                     ),
-                  ),
-                  Container(
-                    width: widget.screenWidth * .15,
-                    child: ElevatedButton(
-                      child: Text(
-                        "add",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 10,
-                          fontWeight: FontWeight.bold,
+                child: Padding(
+                  padding: const EdgeInsets.all(2.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(
+                        width: widget.screenWidth * .4,
+                        child: TextFormField(
+                          maxLines: 1,
+                          controller: _subtaskController,
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.black.withOpacity(.6),
+                            fontWeight: FontWeight.bold,
+                          ),
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          decoration: InputDecoration(
+                            hintText: "Add Tasks",
+                            border: InputBorder.none,
+                            focusedBorder: InputBorder.none,
+                            enabledBorder: InputBorder.none,
+                            errorBorder: UnderlineInputBorder(
+                              borderSide:
+                              BorderSide(color: Colors.red, width: 1),
+                            ),
+                            disabledBorder: InputBorder.none,
+                          ),
                         ),
                       ),
-                      style: ButtonStyle(
-                          backgroundColor:
-                              MaterialStateProperty.all(Colors.black),
-                          overlayColor: MaterialStateProperty.all(
-                              Colors.white.withOpacity(.5))),
-                      onPressed: () {
-                        addSubtask(data);
-                      },
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Container(
-            height: 20,
-          ),
-          Container(
-            height: 240,
-            child: StreamBuilder(
-                stream: _streamSnap,
-                builder: (BuildContext context,
-                    AsyncSnapshot<QuerySnapshot> snapshot) {
-                  if (snapshot.hasError) {
-                    return Text("Error In Fetching Data");
-                  }
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return CupertinoActivityIndicator();
-                  }
-                  if (!snapshot.hasData) {
-                    return Text("You Don't Have Any SubTasks");
-                  }
-                  if (snapshot.hasData) {
-                    if (snapshot.data!.docs.length == 0) {
-                      return Column(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(top: 20.0),
+                      Container(
+                        width: widget.screenWidth * .15,
+                        child: Tooltip(
+                          message: _subtaskController.text==""?"Sub Task Should not Be Empty":"add Sub Task",
+                          child: ElevatedButton(
                             child: Text(
-                              "You Don't Have Any SubTasks Yet",
+                              "add",
                               style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.grey),
+                                color: Colors.white,
+                                fontSize: 10,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
-                          ),
-                        ],
-                      );
-                    } else {
-                      return ListView(
-                          physics: BouncingScrollPhysics(),
-                          children: snapshot.data!.docs
-                              .map((DocumentSnapshot document) {
-                            Map<String, dynamic> datan =
-                                document.data() as Map<String, dynamic>;
-                            return Padding(
-                              padding: const EdgeInsets.all(4.0),
-                              child: Container(
-                                height: 50,
-                                decoration: BoxDecoration(
-                                  shape: BoxShape.rectangle,
-                                  borderRadius: BorderRadius.circular(10),
-                                  color: Colors.white10,
-                                  border:
-                                      Border.all(color: Colors.white, width: 1),
+                            style: ButtonStyle(
+                                backgroundColor:
+                                    MaterialStateProperty.all(Colors.black),
+                                overlayColor: MaterialStateProperty.all(
+                                    Colors.white.withOpacity(.5))),
+                            onPressed: () {
+                              if (_formKey.currentState!.validate()) {
+                                addSubtask(data);
+                              }else{
 
-                                  //borderRadius: BorderRadius.circular(10),
-                                ),
-                                child: Row(
-                                  children: [
-                                    Container(
-                                        height: 20,
-                                        width: 20,
-                                        color: Colors.white,
-                                        child: Center(
-                                            child:
-                                                Text(datan["no"].toString()))),
-                                    Padding(
-                                      padding: const EdgeInsets.only(left: 8.0),
-                                      child: Container(
-                                        width: 200,
-                                          child: SingleChildScrollView(
-                                            scrollDirection: Axis.horizontal,
-                                            child: Text(
-                                        datan["subtask"],
-                                        style: TextStyle(
-                                              color: Colors.black.withOpacity(.5),
-                                              fontWeight: FontWeight.bold),
-                                      ),
-                                          )),
-                                    ),
-                                  ],
+                              }
+                            },
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                height: 20,
+              ),
+              Container(
+                height: 240,
+                child: StreamBuilder(
+                    stream: _streamSnap,
+                    builder: (BuildContext context,
+                        AsyncSnapshot<QuerySnapshot> snapshot) {
+                      if (snapshot.hasError) {
+                        return Text("Error In Fetching Data");
+                      }
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return CupertinoActivityIndicator();
+                      }
+                      if (!snapshot.hasData) {
+                        return Text("You Don't Have Any SubTasks");
+                      }
+                      if (snapshot.hasData) {
+                        if (snapshot.data!.docs.length == 0) {
+                          return Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(top: 20.0),
+                                child: Text(
+                                  "You Don't Have Any SubTasks Yet",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.grey),
                                 ),
                               ),
-                            );
-                          }).toList());
-                    }
-                  }
-                  return Text("error");
-                }),
+                            ],
+                          );
+                        } else {
+                          return ListView(
+                              physics: BouncingScrollPhysics(),
+                              children: snapshot.data!.docs
+                                  .map((DocumentSnapshot document) {
+                                Map<String, dynamic> datan =
+                                    document.data() as Map<String, dynamic>;
+                                return Padding(
+                                  padding: const EdgeInsets.all(4.0),
+                                  child: Container(
+                                    height: 50,
+                                    decoration: BoxDecoration(
+                                      shape: BoxShape.rectangle,
+                                      borderRadius: BorderRadius.circular(10),
+                                      color: Colors.white10,
+                                      border: Border.all(
+                                          color: Colors.white, width: 1),
+
+                                      //borderRadius: BorderRadius.circular(10),
+                                    ),
+                                    child: Row(
+                                      children: [
+                                        Container(
+                                            height: 20,
+                                            width: 20,
+                                            color: Colors.white,
+                                            child: Center(
+                                                child: Text(
+                                                    datan["no"].toString()))),
+                                        Padding(
+                                          padding:
+                                              const EdgeInsets.only(left: 8.0),
+                                          child: Container(
+                                              width: 200,
+                                              child: SingleChildScrollView(
+                                                scrollDirection:
+                                                    Axis.horizontal,
+                                                child: Text(
+                                                  datan["subtask"],
+                                                  style: TextStyle(
+                                                      color: Colors.black
+                                                          .withOpacity(.5),
+                                                      fontWeight:
+                                                          FontWeight.bold),
+                                                ),
+                                              )),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                );
+                              }).toList());
+                        }
+                      }
+                      return Text("error");
+                    }),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
-  addSubtask(Map<String, dynamic> datan)
 
+  addSubtask(Map<String, dynamic> datan) async {
+    int num;
 
-  async {
-    int num = data["numsub"];
-    num = num+1;
-    if(_subtaskController.text!="") {
-      await FirebaseFirestore.instance
-          .collection("users")
-          .doc(FirebaseAuth.instance.currentUser!.uid)
-          .collection("todo")
-          .doc(id)
-          .collection("subtask")
-          .doc(id)
-          .set({
-        "subtask": _subtaskController.text,
-        "no": num,
-      });
-    }
+    await FirebaseFirestore.instance
+        .collection("users")
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .collection("todo")
+        .doc(id)
+        .get()
+        .then((value) async {
+      num = int.parse(value.get("numsub"));
+      if (_subtaskController.text != "") {
+        num = num + 1;
+        const _chars =
+            'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890';
+        Random _rnd = Random.secure();
+
+        String nid = String.fromCharCodes(Iterable.generate(
+            20, (_) => _chars.codeUnitAt(_rnd.nextInt(_chars.length))));
+
+        await FirebaseFirestore.instance
+            .collection("users")
+            .doc(FirebaseAuth.instance.currentUser!.uid)
+            .collection("todo")
+            .doc(id)
+            .update({
+          "numsub": num.toString(),
+        });
+        await FirebaseFirestore.instance
+            .collection("users")
+            .doc(FirebaseAuth.instance.currentUser!.uid)
+            .collection("todo")
+            .doc(id)
+            .collection("subtask")
+            .doc(nid)
+            .set({
+          "subtask": _subtaskController.text,
+          "no": num.toString(),
+        });
+        _subtaskController.text = "";
+      }
+    });
   }
+
 }
